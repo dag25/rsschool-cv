ebf26b99857e0873b4ee4f5f332774a31d020a80
dag <45577604+dag25@users.noreply.github.com>
2024-08-20 22:20:13 +0400

    feat: add burger menu js

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,75 +1,91 @@
-const wrapper = document.querySelector('.wrapper');
-const carousel = document.querySelector('.carousel');
-const firstCardWidth = document.querySelector('.our-friends__item').offsetWidth;
-const arrowBtns = document.querySelectorAll('.our-friends__arrow');
-const carouselChildrens = [...carousel.children];
+console.log(window.location.pathname);
+if (window.location.pathname === '/shelter/index.html') {
+	const wrapper = document.querySelector('.wrapper');
+	const carousel = document.querySelector('.carousel');
+	const firstCardWidth =
+		document.querySelector('.our-friends__item').offsetWidth;
+	const arrowBtns = document.querySelectorAll('.our-friends__arrow');
+	const carouselChildrens = [...carousel.children];
 
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+	let isDragging = false,
+		isAutoPlay = true,
+		startX,
+		startScrollLeft,
+		timeoutId;
 
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+	let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-  carousel.insertAdjacentHTML('afterbegin', card.outerHTML);
-});
+	carouselChildrens
+		.slice(-cardPerView)
+		.reverse()
+		.forEach(card => {
+			carousel.insertAdjacentHTML('afterbegin', card.outerHTML);
+		});
 
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-  carousel.insertAdjacentHTML('beforeend', card.outerHTML);
-});
+	carouselChildrens.slice(0, cardPerView).forEach(card => {
+		carousel.insertAdjacentHTML('beforeend', card.outerHTML);
+	});
 
-carousel.classList.add('no-transition');
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove('no-transition');
+	carousel.classList.add('no-transition');
+	carousel.scrollLeft = carousel.offsetWidth;
+	carousel.classList.remove('no-transition');
 
-arrowBtns.forEach(btn => {
-  btn.addEventListener('click', e => {
-    carousel.scrollLeft += btn.id == 'left' ? -firstCardWidth : firstCardWidth;
-  });
-});
-const dragStart = (e) => {
-  isDragging = true;
-  carousel.classList.add('dragging');
-  startX = e.pageX;
-  startScrollLeft = carousel.scrollLeft;
-};
+	arrowBtns.forEach(btn => {
+		btn.addEventListener('click', e => {
+			carousel.scrollLeft +=
+				btn.id == 'left' ? -firstCardWidth : firstCardWidth;
+		});
+	});
+	const dragStart = e => {
+		isDragging = true;
+		carousel.classList.add('dragging');
+		startX = e.pageX;
+		startScrollLeft = carousel.scrollLeft;
+	};
 
-const dragging = (e) => {
-  if (!isDragging) return;
-  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-};
+	const dragging = e => {
+		if (!isDragging) return;
+		carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+	};
 
-const dragStop = (e) => {
-  isDragging = false;
-  carousel.classList.remove('dragging');
-};
+	const dragStop = e => {
+		isDragging = false;
+		carousel.classList.remove('dragging');
+	};
 
-const infiniteScroll = (e) => {
-  if (carousel.scrollLeft === 0) {
-    carousel.classList.add('no-transition');
-    carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.scrollWidth);
-    carousel.classList.remove('no-transition');
-  } else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-    carousel.classList.add('no-transition');
-    carousel.scrollLeft = carousel.offsetWidth;
-    carousel.classList.remove('no-transition');
-  }
-  clearTimeout(timeoutId);
-  if (!wrapper.matches(':hover')) autoPlay();
-};
+	const infiniteScroll = e => {
+		if (carousel.scrollLeft === 0) {
+			carousel.classList.add('no-transition');
+			carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.scrollWidth;
+			carousel.classList.remove('no-transition');
+		} else if (
+			Math.ceil(carousel.scrollLeft) ===
+			carousel.scrollWidth - carousel.offsetWidth
+		) {
+			carousel.classList.add('no-transition');
+			carousel.scrollLeft = carousel.offsetWidth;
+			carousel.classList.remove('no-transition');
+		}
+		clearTimeout(timeoutId);
+		if (!wrapper.matches(':hover')) autoPlay();
+	};
 
-const autoPlay = () => {
-  if (window.innerWidth < 800 || !isAutoPlay) return;
-  timeoutId = setTimeout(() => {
-    carousel.scrollLeft += firstCardWidth;
-  }, 2500);
-};
-autoPlay();
+	const autoPlay = () => {
+		if (window.innerWidth < 800 || !isAutoPlay) return;
+		timeoutId = setTimeout(() => {
+			carousel.scrollLeft += firstCardWidth;
+		}, 2500);
+	};
+	autoPlay();
 
-carousel.addEventListener('mousedown', dragStart);
-carousel.addEventListener('mousemove', dragging);
-document.addEventListener('mouseup', dragStop);
-carousel.addEventListener('scroll', infiniteScroll);
-wrapper.addEventListener('mouseenter', () => clearTimeout(timeoutId));
-wrapper.addEventListener('mouseleave', autoPlay);
+	carousel.addEventListener('mousedown', dragStart);
+	carousel.addEventListener('mousemove', dragging);
+	document.addEventListener('mouseup', dragStop);
+	carousel.addEventListener('scroll', infiniteScroll);
+	wrapper.addEventListener('mouseenter', () => clearTimeout(timeoutId));
+	wrapper.addEventListener('mouseleave', autoPlay);
+}
+
 
 // Scroll
 const disabledScroll = () => {
@@ -141,7 +157,7 @@ const createModal = (
 
 	const listItem1Element = createElement('li', {
 		className: 'modal__item',
-		innerHTML: `<b>Age:</b> ${age},`,
+		innerHTML: `<b>Age:</b> ${age}`,
 	});
 	const listItem2Element = createElement('li', {
 		className: 'modal__item',
@@ -236,5 +252,37 @@ for (let i = 0; i < productBtns.length; i++) {
           }
 				});
 			});
+  });
+}
+
+// burger
+
+const burgerBtnElement = document.querySelector('.burger');
+const navElement = document.querySelector('.nav');
+const navLinkElements = document.querySelectorAll('.nav__link');
+const overlayElement = document.querySelector('.blackout');
+
+if (burgerBtnElement) {
+		burgerBtnElement.addEventListener('click', () => {
+		document.body.classList.toggle('body--locked');
+    burgerBtnElement.classList.toggle('nav--open');
+    navElement.classList.toggle('nav--open');
+		overlayElement.classList.toggle('overlay');
+  });
+}
+
+if (navLinkElements.length > 0) {
+	navLinkElements.forEach((navLinkElement) => {
+		navLinkElement.addEventListener('click', () => {
+      document.body.classList.remove('body--locked');
+      burgerBtnElement.classList.remove('nav--open');
+      navElement.classList.remove('nav--open');
+			overlayElement.classList.remove('overlay');
+			navLinkElements.forEach((navLinkElement) => {
+				navLinkElement.classList.remove('nav__link--active');
+			});
+			navLinkElement.classList.add('nav__link--active');
+
+		});
   });
 }
